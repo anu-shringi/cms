dcd5b743946ee7651eae5258d737507c139d0b62
anu-shringi <51241355+anu-shringi@users.noreply.github.com>
2023-06-18 04:48:51 +0530

    push local repo

---
+++ contact-management-system 2/src/test/java/com/contact/management/service/contactmanagementsystem/ContactManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contact.management.service.contactmanagementsystem;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
